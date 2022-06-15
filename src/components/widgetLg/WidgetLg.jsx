d47defac3c34372f02112b7d6774df60eefa92a6
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-06-15 20:44:22 +0200

    complete UserList , work on details

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -10,56 +10,58 @@ const WidgetLg = () => {
     <div className="widgetLg">
       <h3 className="title">Latest transactions</h3>
       <table className="widgetTable">
-        <tr className="widgetTr">
-          <th className="widgetTh">Customer</th>
-          <th className="widgetTh">Date</th>
-          <th className="widgetTh">Amount</th>
-          <th className="widgetTh">Status</th>
-        </tr>
-        <tr className="widgetTr">
-          <td className="user">
-            <img className="img" src={avatar} alt="" />
-            <span className="name">Greg Wolf</span>
-          </td>
-          <td className="date">15 Jun 2022</td>
-          <td className="amount">$122.00</td>
-          <td className="status">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetTr">
-          <td className="user">
-            <img className="img" src={avatar} alt="" />
-            <span className="name">Greg Wolf</span>
-          </td>
-          <td className="date">15 Jun 2022</td>
-          <td className="amount">$122.00</td>
-          <td className="status">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetTr">
-          <td className="user">
-            <img className="img" src={avatar} alt="" />
-            <span className="name">Greg Wolf</span>
-          </td>
-          <td className="date">15 Jun 2022</td>
-          <td className="amount">$122.00</td>
-          <td className="status">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetTr">
-          <td className="user">
-            <img className="img" src={avatar} alt="" />
-            <span className="name">Greg Wolf</span>
-          </td>
-          <td className="date">15 Jun 2022</td>
-          <td className="amount">$122.00</td>
-          <td className="status">
-            <Button type="Approved" />
-          </td>
-        </tr>
+        <tbody>
+          <tr className="widgetTr">
+            <th className="widgetTh">Customer</th>
+            <th className="widgetTh">Date</th>
+            <th className="widgetTh">Amount</th>
+            <th className="widgetTh">Status</th>
+          </tr>
+          <tr className="widgetTr">
+            <td className="user">
+              <img className="img" src={avatar} alt="" />
+              <span className="name">Greg Wolf</span>
+            </td>
+            <td className="date">15 Jun 2022</td>
+            <td className="amount">$122.00</td>
+            <td className="status">
+              <Button type="Approved" />
+            </td>
+          </tr>
+          <tr className="widgetTr">
+            <td className="user">
+              <img className="img" src={avatar} alt="" />
+              <span className="name">Greg Wolf</span>
+            </td>
+            <td className="date">15 Jun 2022</td>
+            <td className="amount">$122.00</td>
+            <td className="status">
+              <Button type="Declined" />
+            </td>
+          </tr>
+          <tr className="widgetTr">
+            <td className="user">
+              <img className="img" src={avatar} alt="" />
+              <span className="name">Greg Wolf</span>
+            </td>
+            <td className="date">15 Jun 2022</td>
+            <td className="amount">$122.00</td>
+            <td className="status">
+              <Button type="Pending" />
+            </td>
+          </tr>
+          <tr className="widgetTr">
+            <td className="user">
+              <img className="img" src={avatar} alt="" />
+              <span className="name">Greg Wolf</span>
+            </td>
+            <td className="date">15 Jun 2022</td>
+            <td className="amount">$122.00</td>
+            <td className="status">
+              <Button type="Approved" />
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,126 +1,78 @@
 import './userList.scss';
+import { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'username', headerName: 'Username', width: 130 },
-  { field: 'email', headerName: 'Email', width: 130 },
-  {
-    field: 'status',
-    headerName: 'Status',
-    width: 90,
-  },
-  {
-    field: 'transaction',
-    headerName: 'Transaction Volume',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-  },
-];
-
-const rows = [
-  {
-    id: 1,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 2,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 3,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 4,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 5,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 6,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 7,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 8,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 9,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-  {
-    id: 10,
-    username: 'Jon Snow',
-    avatar:
-      'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    email: 'jon@gmail.com',
-    status: 'active',
-    transaction: '$120.00',
-  },
-];
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { userRows } from '../../dummyData';
+import { Link } from 'react-router-dom';
 
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'user',
+      headerName: 'User',
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="renderUser">
+            <img src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: 'email', headerName: 'Email', width: 200 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 150,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction Volume',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 200,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/${params.row.id}`}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutlineOutlinedIcon
+              className="userDeleteIcon"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="userList">
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
-        pageSize={5}
+        pageSize={10}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        disableSelectionOnClick
+        style={{
+          color: 'white',
+          border: '1px solid #30363d',
+        }}
       />
     </div>
   );
