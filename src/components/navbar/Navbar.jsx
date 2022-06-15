b6543274b6814ce22ead1af7d082752df7973f14
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-06-15 09:18:25 +0200

    add navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,56 @@
 import './navbar.scss';
+import logo from '../../assets/logo.png';
+import avatar from '../../assets/man.png';
+import Badge from '@mui/material/Badge';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topWrapper">
+        <div className="topLeft">
+          <div className="logoContainer">
+            <img src={logo} alt="" />
+          </div>
+          <span className="textLeft">ADMIN PANEL</span>
+        </div>
+        <div className="topRight">
+          <div className="topIconContainer">
+            <Badge
+              badgeContent={1}
+              sx={{
+                '& .MuiBadge-badge': {
+                  color: 'white',
+                  backgroundColor: '#010409',
+                },
+              }}
+            >
+              <NotificationsNoneIcon />
+            </Badge>
+          </div>
+          <div className="topIconContainer">
+            <Badge
+              badgeContent={3}
+              sx={{
+                '& .MuiBadge-badge': {
+                  color: 'white',
+                  backgroundColor: '#010409',
+                },
+              }}
+            >
+              <LanguageIcon />
+            </Badge>
+          </div>
+          <div className="topIconContainer">
+            <SettingsOutlinedIcon />
+          </div>
+          <img className="topAvatar" src={avatar} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
